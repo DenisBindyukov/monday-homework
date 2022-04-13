b9811307b33ae1455b7b3086a2368cca7357af2f
DenisBindyukov <gitgroom@gmail.com>
2022-04-13 12:03:42 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5001;
+
+app.get('/', (req: Request, res: Response ) => {
+    res.send('Hello: World!')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
